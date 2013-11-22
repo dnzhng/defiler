1877d241a3ebaceaed7a53e269c8eaae9deb1027
thedanzhang <dyz2@duke.edu>
2013-11-21 20:33:08 -0500

    Implement start request

--- src/defiler/defilerVirtualDisk.java
+++ src/defiler/defilerVirtualDisk.java
@@ -2,6 +2,7 @@ package defiler;
 
 import java.io.IOException;
 
+import common.Constants;
 import common.Constants.DiskOperationType;
 
 import dblockcache.DBuffer;
@@ -13,7 +14,12 @@ public class defilerVirtualDisk extends VirtualDisk {
 	public void startRequest(DBuffer buf, DiskOperationType operation)
 			throws IllegalArgumentException, IOException {
 		// TODO Auto-generated method stub
-		
+		if (operation == Constants.DiskOperationType.READ) {
+			readBlock(buf);
+		}
+		else if (operation == Constants.DiskOperationType.WRITE) {
+			writeBlock(buf);
+		}
 	}
 	
 	
