40c61b409dab53d64136e39ee561deb87ff60ef9
Scott Valentine <westwooster@gmail.com>
2013-12-02 12:14:04 -0500

    added unit test for INode r/w

---
+++ src/test/INodeIO.java
@@ -0,0 +1,42 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import common.DFileID;
+import dfs.INode;
+
+public class INodeIO {
+
+	@Test
+	public void test() {
+		
+		int id = 4;
+		boolean valid = true;
+		int size = 746323;
+		int start = 32;
+		
+		
+		INode in = new INode(new DFileID(id), valid, size, start);
+		
+		byte[] data = in.toByteArray();
+		INode read;
+		try {
+			read = new INode(data);
+		} catch (IOException e) {
+			fail();
+			e.printStackTrace();
+			return;
+		}
+		
+		assertEquals(id, read.getDFileID().getDFileID());
+		assertEquals(valid, read.isValid());
+		assertEquals(size, read.getFileSize());
+		assertEquals(start, read.getHeadBlock());
+		
+	}
+
+}
