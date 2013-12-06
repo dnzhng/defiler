30385e2494fa1d476736dcf45c28cf3359b2324d
Scott Valentine <westwooster@gmail.com>
2013-12-06 14:58:37 -0500

    cache tests... not complete

---
+++ src/test/CacheTest.java
@@ -0,0 +1,62 @@
+package test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import dblockcache.BufferCache;
+import dblockcache.DBuffer;
+import dblockcache.DBufferCache;
+
+import virtualdisk.IVirtualDisk;
+import virtualdisk.VD;
+
+public class CacheTest {
+
+	
+	
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException{
+		IVirtualDisk vd = new VD("scot",false);
+		
+		DBufferCache cache = new BufferCache(1, vd);
+		DBuffer x = cache.getBlock(4);
+		
+		byte[] bufferw = {4,4};
+		
+		x.write(bufferw, 0, 2);
+		
+		cache.releaseBlock(x);
+		
+		x = cache.getBlock(1);
+		
+		byte[] writ2 = {1,1};
+		
+		x.write(writ2, 0, 2);
+		
+		cache.releaseBlock(x);
+		
+		cache.sync();
+		
+
+		
+		cache = new BufferCache(1, vd);
+		
+		byte[] bufferr = new byte[2];
+		DBuffer y = cache.getBlock(1);
+		
+		y.read(bufferr, 0, 2);
+		
+		for(int i = 0; i < bufferr.length; ++i){
+			System.out.print(bufferr[i] + " ");
+		}
+		cache.releaseBlock(y);
+		y = cache.getBlock(4);
+		
+		y.read(bufferr, 0, 2);
+		
+		for(int i = 0; i < bufferr.length; ++i){
+			System.out.print(bufferr[i] + " ");
+		}
+		
+	}
+}
